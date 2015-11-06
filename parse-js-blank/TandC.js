b29915ee72002760bc7d41ec82448db51f47fdcc
Jason Li <jason.li@wustl.edu>
2015-11-06 11:25:06 -0600

    first

---
+++ parse-js-blank/TandC.js
@@ -0,0 +1,10 @@
+function allChecked(event){
+	var check1 = $("#check1").checked;
+	var check2 = $("#check1").checked;
+	var check3 = $("#check1").checked;
+	var check4 = $("#check1").checked;
+	var checkAccept = $("#check1").checked;
+	if (check1 && check2 && check3 && check4 && checkAccept){
+		
+	}
+}
