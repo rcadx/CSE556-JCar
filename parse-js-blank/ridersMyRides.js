d64d0415b521c7b318dbddc328b4c03d755b3306
Jason Li <jason.li@wustl.edu>
2015-11-09 19:26:36 -0600

    progress on riders my rides page

--- parse-js-blank/ridersMyRides.js
+++ parse-js-blank/ridersMyRides.js
@@ -9,14 +9,21 @@ weekday[4] = "Thursday";
 weekday[5] = "Friday";
 weekday[6] = "Saturday";
 
-var ridesArr = new Array();
-var bookedRidesArr = new Array();
+var ridesArr = new Array(); //non booked rides in Ride Requests table
+var bookedRidesArr = new Array(); //booked rides in Ride Requests table
+var bookedDriverSubmittedRides = new Array(); //booked rides in Rides table (holds Driver's submitted rides)
 
 $("#backToHome").on("click", function() {
 	window.location.replace("riders.html");
 });
 
+$(document).on("click", '.unBookRide', function() {
+	var id = $(this).attr("id");
+	unBookRide(id);
+});
+
 $(document).ready(function() {
+	//Get all ride requests the user made
 	var Rides = Parse.Object.extend("RideRequests");
 	var query = new Parse.Query(Rides);
 	query.equalTo("createdBy", Parse.User.current());
@@ -26,7 +33,7 @@ $(document).ready(function() {
 			for (var i = 0; i < rides.length; i++) {
 				var ride = rides[i];
 				var driver = ride.get("driver");
-				if (!driver) {
+				if (!driver) { 
 					ridesArr.push(ride);
 				} 
 				else {
@@ -35,7 +42,36 @@ $(document).ready(function() {
 			
 			}
 			displayRides();
-			displayBookedRides();
+			displayAcceptedRideRequestsList();
+		},
+		error: function(error) {
+			console.log("No rides found");
+		}
+	});
+
+	//Get all the driver submitted rides that the user booked
+	var Rides2 = Parse.Object.extend("Rides");
+	var query2 = new Parse.Query(Rides2);
+	query2.include("createdBy");
+	query2.include("riders");
+	query2.find({
+		success: function(rides) {
+			for (var i = 0; i < rides.length; i++) {
+				var ride = rides[i];
+				var riders = ride.get("riders");
+				var booked = false;
+				for (var j = 0; j < riders.length; j++) {
+					var user = Parse.User.current();
+					var rider = riders[j];
+					if (rider.id == user.id) {
+						booked = true;
+					}
+				}
+				if (booked) {
+					bookedDriverSubmittedRides.push(ride);
+				}
+			}
+			displayBookedDriverSubmittedRides();
 		},
 		error: function(error) {
 			console.log("No rides found");
@@ -72,12 +108,12 @@ function displayRides() {
 		//Outer Div
 		var div = "<div class=ride>" + dateNode + destinationNode + priceNode + pickupNode + "</div><br><br>";
 			
-		$("#myRidesList").append(div);
+		$("#pendingRideRequestsList").append(div);
 	}
 		
 }
 
-function displayBookedRides() {
+function displayAcceptedRideRequestsList() {
 for (var i = 0; i < bookedRidesArr.length; i++) {
 		//Date and time
 		var ride = bookedRidesArr[i];
@@ -96,7 +132,7 @@ for (var i = 0; i < bookedRidesArr.length; i++) {
 
 		//Price
 		var price = ride.get("price");
-		var priceNode = "<p id=price>Price: " + price + "</p>";
+		var priceNode = "<p id=price>Requested Price: " + price + "</p>";
 
 		//Pickup Location
 		var pickupLoc = ride.get("pickupLoc");
@@ -105,6 +141,88 @@ for (var i = 0; i < bookedRidesArr.length; i++) {
 		//Outer Div
 		var div = "<div class=ride>" + dateNode + destinationNode + priceNode + pickupNode + "</div><br><br>";
 			
-		$("#bookedRidesList").append(div);
+		$("#acceptedRideRequestsList").append(div);
 	}
+}
+
+function displayBookedDriverSubmittedRides() {
+	for (var i = 0; i < bookedDriverSubmittedRides.length; i++) {
+			var ride = bookedDriverSubmittedRides[i];
+
+			//Date and time
+			var date = new Date(ride.get("date"));
+			var time = date.toLocaleTimeString();
+			var day = weekday[date.getDay()];
+			var dateNode = "<p id=date><b>Date:</b> " + day + ", " + (date.getMonth() + 1) + "/" + (date.getDate()) + "/" + (date.getFullYear()) + " at " + time + "</p>";
+
+			//Destination
+			var destination = ride.get("destination");
+			var destinationNode = "<p id='destination'><b>Destination:</b> " + destination + "</p>";
+
+			//Price
+			var price = ride.get("price");
+			var priceNode = "<p id='price'><b>Price:</b> " + price + "</p>";
+
+			//Available seats
+			var seats = ride.get("numSeats");
+			var seatsNode = "<p id='numSeats'><b>Seats Available:</b> " + seats + "</p>";
+
+			//Pickup Location
+			var pickupLoc = ride.get("pickupLoc");
+			var pickupNode = "<p id='pickupLoc'><b>Pickup Location:</b> " + pickupLoc + "</p>";
+
+			//Driver rating
+			var driver = ride.get("createdBy");
+			var driverRating = Number(driver.get("rating"));
+			var stars = "";
+			for (var j = 0; j < driverRating; j++) {
+				stars += "<img src='Star.png'>"
+			}
+			var driverRatingNode = driverRating ? ("<p id=rating><b>Driver Rating:</b> " + stars + "</p>") : "<p>Driver Rating: This driver has no ratings yet.</p>"
+
+			//Facebook Picture of Driver
+			var driverFBID = driver.get("fbID");
+			var driverProfPicURL = "'https://graph.facebook.com/" + driverFBID + "/picture?type=normal'";
+			var driverProfPicNode = "<img src=" + driverProfPicURL + "><br>";
+
+			//Name of Driver
+			var driverName = driver.get("firstName") + " " + driver.get("lastName");
+			var driverNameNode = "<p id='name'><b>Driver's Name:</b> " + driverName + "</p><br>"
+
+			//See if the ride has been booked already by you
+			var riders = ride.get("riders");
+			var booked = false;
+			for (var j = 0; j < riders.length; j++) {
+				var user = Parse.User.current();
+				var rider = riders[j];
+				if (rider.id == user.id) {
+					booked = true;
+				}
+			}
+
+			var button = "";
+			if (booked) {
+				button = "<button class='unBookRide' id='" + ride.id + "'>UNBOOK RIDE</button>"
+			} else {
+				button = "<button class='bookRide' id='" + ride.id + "'>BOOK RIDE</button>";
+			}
+		
+			//Outer Div
+			var div = "<div class='ride' id='" + ride.id + "' style='border-style: solid; border-color: " + "black" + "'>" + driverProfPicNode + driverName + dateNode + destinationNode + priceNode + seatsNode + pickupNode + driverRatingNode + button + "</div><br><br>";
+			
+			$("#bookedRidesList").append(div);
+		}	
+}
+
+function unBookRide(id) {
+	var Rides = Parse.Object.extend("Rides");
+	var ride = new Rides();
+	ride.id = id;
+	ride.remove("riders", Parse.User.current());
+	ride.increment("numSeats", 1);
+	ride.save();
+
+	alert("You have unbooked this ride!");
+	
+	location.reload();
 }
