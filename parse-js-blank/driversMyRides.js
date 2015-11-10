660e63f95ef9a19ce5d844bdd3771cfc443d39ee
Jason Li <jason.li@wustl.edu>
2015-11-09 20:08:53 -0600

    finished both drivers/riders my rides page.

--- parse-js-blank/driversMyRides.js
+++ parse-js-blank/driversMyRides.js
@@ -16,6 +16,16 @@ $("#backToHome").on("click", function() {
 	window.location.replace("drivers.html");
 });
 
+$(document).on("click", '.unBookRide', function() {
+	var id = $(this).attr("id");
+	unBookRide(id);
+});
+
+$(document).on("click", '.cancelRide', function() {
+	var id = $(this).attr("id");
+	cancelRide(id);
+});
+
 $(document).ready(function() {
 	
 	var Rides = Parse.Object.extend("Rides");
@@ -28,7 +38,7 @@ $(document).ready(function() {
 				var ride = rides[i];
 				ridesArray.push(ride);
 			}
-			displayRides(ridesArray, false);
+			displayRides();
 		},
 		error: function(error) {
 			console.log("No rides found");
@@ -36,15 +46,16 @@ $(document).ready(function() {
 	});
 
 	var Rides = Parse.Object.extend("RideRequests");
-	var query = new Parse.Query(Rides);
-	query.equalTo("driver", Parse.User.current());
-	query.find({
+	var query2 = new Parse.Query(Rides);
+	query2.equalTo("driver", Parse.User.current());
+	query2.include("createdBy");
+	query2.find({
 		success: function(rides) {
 			for (var i = 0; i < rides.length; i++) {
 				var ride = rides[i];
 				acceptedRidesArray.push(ride);
 			}
-			displayRides(acceptedRidesArray, true);
+			displayAcceptedRides();
 		},
 		error: function(error) {
 			console.log("No rides found");
@@ -53,10 +64,10 @@ $(document).ready(function() {
 
 });
 
-function displayRides(ridesArr, accepted) { //accepted parameter is flag for whether to append ride to acceptedRidesList or myRidesList in html
-	for (var i = 0; i < ridesArr.length; i++) {
+function displayRides() { //accepted parameter is flag for whether to append ride to acceptedRidesList or myRidesList in html
+	for (var i = 0; i < ridesArray.length; i++) {
 		//Date and time
-		var ride = ridesArr[i];
+		var ride = ridesArray[i];
 
 		var date = new Date(ride.get("date"));
 		var time = date.toLocaleTimeString();
@@ -79,14 +90,107 @@ function displayRides(ridesArr, accepted) { //accepted parameter is flag for whe
 		var pickupLoc = ride.get("pickupLoc");
 		var pickupNode = "<p id=price>Pickup Location: " + pickupLoc + "</p>";
 
+		//cancel button
+		var button = "<button class='cancelRide' id='" + ride.id + "'>CANCEL RIDE</button>";
+
 		//Outer Div
-		var div = "<div class=ride>" + dateNode + destinationNode + priceNode + seatsNode + pickupNode + "</div><br><br>";
+		var div = "<div class=ride>" + dateNode + destinationNode + priceNode + seatsNode + pickupNode + button + "</div><br><br>";
 			
-		if (accepted) {
-			$("#acceptedRidesList").append(div);
+		$("#myRidesList").append(div);
+	}		
+}
+
+function displayAcceptedRides() {
+	for (var i = 0; i < acceptedRidesArray.length; i++) {
+		var ride = acceptedRidesArray[i];
+
+		//Date and time
+		var start = new Date(ride.get("startTime"));
+		var end = new Date(ride.get("endTime"));
+		var startTime = start.toLocaleTimeString();
+		var endTime = end.toLocaleTimeString();
+		var date = new Date(ride.get("date"));
+		var day = weekday[date.getDay()];
+		var dateNode = "<p id=date><b>Date:</b> " + day + ", " + (date.getMonth() + 1) + "/" + (date.getDate()) + "/" + (date.getFullYear()) + " from " + startTime + " to " + endTime + "</p>";
+
+		//Destination
+		var destination = ride.get("destination");
+		var destinationNode = "<p id='destination'><b>Destination:</b> " + destination + "</p>";
+
+		//Price
+		var price = ride.get("price");
+		var priceNode = "<p id=price><b>Requested Price:</b> " + price + "</p>";
+
+		//Pickup Location
+		var pickupLoc = ride.get("pickupLoc");
+		var pickupNode = "<p id='pickupLoc'><b>Requested Pickup Location:</b> " + pickupLoc + "</p>";
+
+		//Seats Needed
+		var numSeats = ride.get("numSeats");
+		var numSeatsNode = "<p id='numSeats'><b>Number Seats Requested:</b> " + numSeats + "</p>";
+
+		//Facebook Picture of Driver
+		var rider = ride.get("createdBy");
+		var riderFBID = rider.get("fbID");
+		var riderProfPicURL = "https://graph.facebook.com/" + riderFBID + "/picture?type=normal";
+		var riderProfPicNode = "<img src=" + riderProfPicURL + "><br>";
+
+		//Name of Driver
+		var riderName = rider.get("firstName") + " " + rider.get("lastName");
+		var riderNameNode = "<p id='name'><b>Rider's Name:</b> " + riderName + "</p>"
+
+		//See if the ride has been booked already by you
+		var driver = ride.get("driver");
+		var user = Parse.User.current();
+		var booked = (driver && driver.id == user.id);
+
+		//I can Drive button
+		var button = "";
+		if (!booked) {
+			button = "<button class='bookRide' id='" + ride.id + "'>I CAN DRIVE</button>"
 		} else {
-			$("#myRidesList").append(div);
+			button = "<button class='unBookRide' id='" + ride.id + "'>I CAN NO LONGER DRIVE</button>";
 		}
+
+		//Outer Div
+		var div = "<div class='ride' style='border-style: solid; border-color: " + "black" + "'>" + riderProfPicNode + riderNameNode + dateNode + destinationNode + priceNode + pickupNode + numSeatsNode + button + "</div><br><br>";
+			
+		$("#acceptedRidesList").append(div);
 	}
-		
+
+}
+
+function unBookRide(id) {
+	var Rides = Parse.Object.extend("RideRequests");
+	var ride = new Rides();
+	ride.id = id;
+	ride.unset("driver");
+	ride.save();
+
+	alert("You have successfully updated that you can no longer drive this ride!");
+	
+	location.reload();
+}
+
+function cancelRide(id) {
+	var Rides = Parse.Object.extend("Rides");
+	var query = new Parse.Query(Rides);
+	query.get(id, {
+  		success: function(myObj) {
+	    // The object was retrieved successfully.
+	    myObj.destroy({
+	    	success: function(myObj) {
+	    		alert("You have successfully canceled this ride!");
+	    		location.reload();
+	    	},
+	    	error: function(myObj) {
+
+	    	}
+	    });
+	  },
+	  error: function(object, error) {
+	    // The object was not retrieved successfully.
+	    // error is a Parse.Error with an error code and description.
+	  }
+	});
 }
