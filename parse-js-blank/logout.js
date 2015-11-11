ffbd56f27b82d73fb8392cec4112f06d7d58e7e3
abctwinkie <cameronwong203@yahoo.com>
2015-11-11 10:53:04 -0600

    no message

--- parse-js-blank/logout.js
+++
@@ -1,6 +0,0 @@
-Parse.initialize("eamcKJmUTepWXqQzYx5iNmgVUcX55xvCQX749IfY", "Gg3ZOGnMTkHaMIOZ0OJHsig6QwU5j8Jev2RkIZML");
-
-$("#logoutBtn").on("click", function() {
-	Parse.User.logOut();
-	window.location.replace("login.html");
-});
